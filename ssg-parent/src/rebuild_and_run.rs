eeddd970f625f910cf4bcb4acc0f91a272415382
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-06-22 13:46:42 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-parent/src/rebuild_and_run.rs

--- ssg-parent/src/rebuild_and_run.rs
+++ ssg-parent/src/rebuild_and_run.rs
@@ -1,5 +1,6 @@
 use thiserror::Error;
 use tokio::process::Command;
+use watchexec::{config::{InitConfig, RuntimeConfig}, ErrorHook};
 
 #[derive(Error, Debug)]
 pub enum WatchError {
@@ -10,7 +11,16 @@ pub enum WatchError {
 }
 
 pub async fn watch_for_changes_and_rebuild() -> WatchError {
+    let mut init_config = InitConfig::default();
 
+    init_config.on_error(|error: ErrorHook| async {
+        eprintln!("{}", error.error);
+        Ok(())
+    });
+
+    let runtime_config = RuntimeConfig::default();
+
+    runtime_config.pathset(["builder"]);
 
     WatchError::Exit(status)
 }
