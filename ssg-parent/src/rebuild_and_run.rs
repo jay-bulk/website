4070eb936b08129297f3482fef10d79207431c79
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-06-22 13:31:09 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-parent/src/rebuild_and_run.rs

--- ssg-parent/src/rebuild_and_run.rs
+++ ssg-parent/src/rebuild_and_run.rs
@@ -10,27 +10,7 @@ pub enum WatchError {
 }
 
 pub async fn watch_for_changes_and_rebuild() -> WatchError {
-    let child = Command::new("cargo")
-        .args([
-            "bin",
-            "cargo-watch",
-            "--workdir",
-            "builder",
-            "--exec",
-            "run",
-        ])
-        .spawn();
 
-    let mut child = match child {
-        Ok(child) => child,
-        Err(err) => return err.into(),
-    };
-
-    // success case is indefinitely waiting here
-    let status = match child.wait().await {
-        Ok(status) => status,
-        Err(err) => return err.into(),
-    };
 
     WatchError::Exit(status)
 }
