1bb55611bf76bf273b8b26a8b6266aaed267528d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-07-03 11:13:49 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-parent/src/dev.rs

--- ssg-parent/src/dev.rs
+++ ssg-parent/src/dev.rs
@@ -16,10 +16,19 @@ pub enum DevError {
 }
 
 pub async fn dev<O: AsRef<Utf8Path>>(launch_browser: bool, output_dir: O) -> DevError {
-    tokio::select! {
-        error = watch_for_changes_and_rebuild() => { DevError::Watch(error) },
-        error = start_development_web_server(launch_browser, Utf8PathBuf::from(output_dir.as_ref())) => { DevError::Io(error) },
-    }
+
+    let outputs = app(inputs);
+
 }
 
-fn app(sources:Sources)
+struct Inputs {
+
+}
+
+struct Outputs {
+}
+
+fn app(inputs: Inputs) -> Outputs {
+    todo!()
+}
+
