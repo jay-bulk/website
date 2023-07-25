e6266be4fd7b9df9a3b40afcff176a603eda5fda
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-07-25 21:37:35 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:reactive/src/driver/child_process_killer.rs

--- reactive/src/driver/child_process_killer.rs
+++ reactive/src/driver/child_process_killer.rs
@@ -1,4 +1,9 @@
-struct ChildProcessKillerDriver(mpsc::Sender<Result<(), std::io::Error>>);
+use futures::{channel::mpsc, future::LocalBoxFuture, stream::LocalBoxStream, SinkExt, StreamExt};
+use tokio::process::Child;
+
+use super::Driver;
+
+pub struct ChildProcessKillerDriver(mpsc::Sender<Result<(), std::io::Error>>);
 
 impl Driver for ChildProcessKillerDriver {
     type Init = ();
@@ -7,28 +12,13 @@ impl Driver for ChildProcessKillerDriver {
 
     fn new(_init: Self::Init) -> (Self, Self::Output) {
         let (sender, mut receiver) = mpsc::channel(1);
-
-        let stream = fn_stream(|emitter| async move {
-            loop {
-                let value = receiver.recv().await.expect(NEVER_ENDING_STREAM);
-                emitter.emit(value).await;
-            }
-        })
-        .boxed();
-
         let child_killer_driver = Self(sender);
-
-        (child_killer_driver, stream)
+        (child_killer_driver, receiver.boxed_local())
     }
 
     fn init(self, mut kill_child: Self::Input) -> LocalBoxFuture<'static, ()> {
-        async move {
-            loop {
-                let mut child = kill_child.next().await.expect(NEVER_ENDING_STREAM);
-                let result = child.kill().await;
-                self.0.send(result).await.unwrap();
-            }
-        }
-        .boxed_local()
+        let Self(sender) = self;
+        let kill_child = kill_child.map(|child| Ok(child.kill())).boxed_local();
+        sender.send_all(&mut kill_child)
     }
 }
