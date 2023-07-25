2e7fba1504498b0db2e7c25f3a6c7c6999dab218
José Manuel Peña <josemanuelp2@gmail.com>
2023-07-25 11:52:55 -0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-parent/src/dev.rs

--- reactive/src/driver/child_process_killer.rs
+++ reactive/src/driver/child_process_killer.rs
@@ -1,4 +1,6 @@
-use futures::{channel::mpsc, future::LocalBoxFuture, stream::LocalBoxStream, SinkExt, StreamExt, FutureExt};
+use futures::{
+    channel::mpsc, future::LocalBoxFuture, stream::LocalBoxStream, FutureExt, SinkExt, StreamExt,
+};
 use tokio::process::Child;
 
 use super::Driver;
@@ -11,14 +13,19 @@ impl Driver for ChildProcessKillerDriver {
     type Output = LocalBoxStream<'static, Result<(), std::io::Error>>;
 
     fn new(_init: Self::Init) -> (Self, Self::Output) {
-        let (sender, mut receiver) = mpsc::channel(1);
+        let (sender, receiver) = mpsc::channel(1);
         let child_killer_driver = Self(sender);
         (child_killer_driver, receiver.boxed_local())
     }
 
     fn init(self, kill_child: Self::Input) -> LocalBoxFuture<'static, ()> {
         let Self(mut sender) = self;
-        let mut kill_child = kill_child.then(|mut child| async move {Ok(child.kill().await)}).boxed_local();
-        async move{sender.send_all(&mut kill_child).map(Result::unwrap).await}.boxed_local()
+        let mut kill_child = kill_child
+            .then(|mut child| async move { Ok(child.kill().await) })
+            .boxed_local();
+        async move {
+            sender.send_all(&mut kill_child).map(Result::unwrap).await;
+        }
+        .boxed_local()
     }
 }
--- ssg-parent/src/dev.rs
+++ ssg-parent/src/dev.rs
@@ -12,7 +12,7 @@ use futures::{
 };
 use notify::{recommended_watcher, Event, EventKind, RecursiveMode, Watcher};
 use portpicker::Port;
-use reactive::driver::{EprintlnDriver, StaticCommandDriver, Driver};
+use reactive::driver::{Driver, EprintlnDriver, StaticCommandDriver};
 use thiserror::Error;
 use tokio::{
     process::{Child, Command},
@@ -352,41 +352,6 @@ fn app(inputs: Inputs) -> Outputs {
         launch_browser,
         error,
         some_task,
-    }
-}
-
-struct ChildKillerDriver(mpsc::Sender<Result<(), std::io::Error>>);
-
-impl Driver for ChildKillerDriver {
-    type Init = ();
-    type Input = LocalBoxStream<'static, Child>;
-    type Output = LocalBoxStream<'static, Result<(), std::io::Error>>;
-
-    fn new(_init: Self::Init) -> (Self, Self::Output) {
-        let (sender, mut receiver) = mpsc::channel(1);
-
-        let stream = fn_stream(|emitter| async move {
-            loop {
-                let value = receiver.recv().await.expect(NEVER_ENDING_STREAM);
-                emitter.emit(value).await;
-            }
-        })
-        .boxed();
-
-        let child_killer_driver = Self(sender);
-
-        (child_killer_driver, stream)
-    }
-
-    fn init(self, mut kill_child: Self::Input) -> LocalBoxFuture<'static, ()> {
-        async move {
-            loop {
-                let mut child = kill_child.next().await.expect(NEVER_ENDING_STREAM);
-                let result = child.kill().await;
-                self.0.send(result).await.unwrap();
-            }
-        }
-        .boxed_local()
     }
 }
 
