baada75a8d38b5d8b6d5dbf7fc37c9d43d971406
José Manuel Peña <josemanuelp2@gmail.com>
2023-07-26 11:25:08 -0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:reactive/src/driver/open_that.rs

--- reactive/src/driver/open_that.rs
+++ reactive/src/driver/open_that.rs
@@ -8,22 +8,34 @@ use futures::{
 
 use super::Driver;
 
-pub struct StaticOpenThatDriver<O : AsRef<OsStr>> { os_str: O, sender: Sender<std::io::Result<()>> }
+pub struct StaticOpenThatDriver<O: AsRef<OsStr>> {
+    os_str: O,
+    sender: Sender<std::io::Result<()>>,
+}
 
-impl<O : AsRef<OsStr>> Driver for StaticOpenThatDriver<O> {
+impl<O: AsRef<OsStr>> Driver for StaticOpenThatDriver<O> {
     type Init = O;
-    type Input = LocalBoxStream<'static, Box<dyn AsRef<OsStr>>>;
+    type Input = LocalBoxStream<'static, ()>;
     type Output = LocalBoxStream<'static, std::io::Result<()>>;
 
     fn new(init: Self::Init) -> (Self, Self::Output) {
         let (sender, receiver) = mpsc::channel(1);
-        (Self{os_str: O,sender}, receiver.boxed_local())
+        (
+            Self {
+                os_str: init,
+                sender,
+            },
+            receiver.boxed_local(),
+        )
     }
 
     fn init(self, input: Self::Input) -> futures::future::LocalBoxFuture<'static, ()> {
-        let Self(mut sender) = self;
+        let Self {
+            ref os_str,
+            mut sender,
+        } = self;
         let mut opened = input
-            .then(|that| futures::future::ready(Ok(open::that(that.as_ref()))))
+            .then(move |_| futures::future::ready(Ok(open::that(os_str.as_ref()))))
             .boxed_local();
         async move {
             sender.send_all(&mut opened).map(Result::unwrap).await;
