811b4b3aebd661c147ab94cb633b58ea8588c3d3
Remy Clarke <60509493+ClarkeRemy@users.noreply.github.com>
2023-07-26 22:07:22 +0900

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:reactive/src/driver/open_url.rs

--- reactive/src/driver/open_url.rs
+++ reactive/src/driver/open_url.rs
@@ -1,20 +1,23 @@
-struct OpenUrlDriver(CompleteHandle<Result<(), std::io::Error>>);
 
-impl Driver for OpenUrlDriver {
-    type Init = ();
-    type Input = LocalBoxFuture<'static, Url>;
-    type Output = LocalBoxFuture<'static, Result<(), std::io::Error>>;
-    fn new(_init: Self::Init) -> (Self, Self::Output) {
-        let (future, handle) = future_handles::sync::create();
-        (Self(handle), future.map(Result::unwrap).boxed_local())
-    }
 
-    fn init(self, url: Self::Input) -> LocalBoxFuture<'static, ()> {
-        async move {
-            self.0.complete(open::that(url.await.as_str()));
-            future::pending::<()>().await;
-        }
-        .boxed_local()
-    }
-}
+
+// struct OpenUrlDriver(CompleteHandle<Result<(), std::io::Error>>);
+
+// impl Driver for OpenUrlDriver {
+//     type Init = ();
+//     type Input = LocalBoxFuture<'static, Url>;
+//     type Output = LocalBoxFuture<'static, Result<(), std::io::Error>>;
+//     fn new(_init: Self::Init) -> (Self, Self::Output) {
+//         let (future, handle) = future_handles::sync::create();
+//         (Self(handle), future.map(Result::unwrap).boxed_local())
+//     }
+
+//     fn init(self, url: Self::Input) -> LocalBoxFuture<'static, ()> {
+//         async move {
+//             self.0.complete(open::that(url.await.as_str()));
+//             future::pending::<()>().await;
+//         }
+//         .boxed_local()
+//     }
+// }
 
