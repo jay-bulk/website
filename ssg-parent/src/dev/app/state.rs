8054f970de6d8a250d8e551ce1a30ab03a25cacc
Remy Clarke <60509493+ClarkeRemy@users.noreply.github.com>
2023-08-08 23:05:35 +0900

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-parent/src/dev/app.rs

--- ssg-parent/src/dev/app/state.rs
+++ ssg-parent/src/dev/app/state.rs
@@ -3,19 +3,14 @@ pub(super) struct State {
     builder: BuilderState,
 }
 impl State {
-    pub(super) fn input_event(
-        &mut self,
-        input: super::InputEvent,
-    ) -> Option<super::OutputEvent> {
+    pub(super) fn input_event(&mut self, input: super::InputEvent) -> Option<super::OutputEvent> {
         match input {
             super::InputEvent::BuilderKilled(result) => self.builder_killed(result),
             super::InputEvent::Notify(result) => self.fs_change(result),
             super::InputEvent::BuilderStarted(child) => self.builder_started(child),
             super::InputEvent::BrowserOpened(result) => match result {
                 Ok(_) => None,
-                Err(error) => {
-                    Some(super::OutputEvent::Error(super::super::DevError::Io(error)))
-                }
+                Err(error) => Some(super::OutputEvent::Error(super::super::DevError::Io(error))),
             },
             super::InputEvent::ServerError(error) => {
                 Some(super::OutputEvent::Error(super::super::DevError::Io(error)))
@@ -24,10 +19,7 @@ impl State {
     }
 
     #[allow(clippy::unnecessary_wraps)]
-    fn builder_killed(
-        &mut self,
-        result: Result<(), std::io::Error>,
-    ) -> Option<super::OutputEvent> {
+    fn builder_killed(&mut self, result: Result<(), std::io::Error>) -> Option<super::OutputEvent> {
         match result {
             Ok(_) => {
                 self.builder = BuilderState::None;
@@ -45,19 +37,17 @@ impl State {
             Ok(event) => match event.kind {
                 reactive::driver::notify::EventKind::Create(_)
                 | reactive::driver::notify::EventKind::Modify(_)
-                | reactive::driver::notify::EventKind::Remove(_) => {
-                    match &mut self.builder {
-                        BuilderState::Starting => {
-                            self.builder = BuilderState::Obsolete;
-                            None
-                        }
-                        BuilderState::Started(_) => {
-                            let child = self.builder.killing().unwrap();
-                            Some(super::OutputEvent::KillChildProcess(child))
-                        }
-                        BuilderState::None | BuilderState::Obsolete => None,
+                | reactive::driver::notify::EventKind::Remove(_) => match &mut self.builder {
+                    BuilderState::Starting => {
+                        self.builder = BuilderState::Obsolete;
+                        None
                     }
-                }
+                    BuilderState::Started(_) => {
+                        let child = self.builder.killing().unwrap();
+                        Some(super::OutputEvent::KillChildProcess(child))
+                    }
+                    BuilderState::None | BuilderState::Obsolete => None,
+                },
                 _ => None,
             },
             Err(error) => Some(super::OutputEvent::Error(super::super::DevError::Notify(
@@ -110,4 +100,3 @@ impl BuilderState {
         }
     }
 }
-
