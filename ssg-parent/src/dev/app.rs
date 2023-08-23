174068581f0f9e14950395b5ecb91320ec06f7db
Remy Clarke <60509493+ClarkeRemy@users.noreply.github.com>
2023-08-24 00:04:30 +0900

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-parent/src/dev/app.rs

--- ssg-parent/src/dev/app.rs
+++ ssg-parent/src/dev/app.rs
@@ -83,17 +83,12 @@ pub(super) fn app(inputs: Inputs) -> Outputs {
             .boxed_local(),
         browser_launch.map(InputEvent::BrowserOpened).boxed_local(),
     ])
-    .inspect(|input| {
-        dbg!(input);
-    })
     .scan(state::State::default(), move |state, input| {
         future::ready(Some(state.input_event(input)))
     })
     .filter_map(future::ready);
 
-    let mut output = initial.chain(reaction).inspect(|output| {
-        dbg!(output);
-    });
+    let mut output = initial.chain(reaction);
 
     let (mut kill_child_sender, kill_child) = mpsc::channel(1);
     let (mut run_builder_sender, run_builder) = mpsc::channel(1);
@@ -104,7 +99,6 @@ pub(super) fn app(inputs: Inputs) -> Outputs {
     let stream_splitter_task = async move {
         loop {
             let event = output.next().await.unwrap();
-            println!("after await");
             match event {
                 OutputEvent::Stderr(output) => {
                     stderr_sender.send(output).await.unwrap();
